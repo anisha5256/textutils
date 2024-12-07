8b4727f66287592974838422e78e048b042059d8
Anisha kumari <anishaanne789@gmail.com>
2024-12-07 23:11:29 +0530

    update the link in Home Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
@@ -42,11 +42,20 @@ function App() {
     document.title='TextUtils - Light Mode';
   }
   }
-  return ( <Router> <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+  return ( 
+    <>
+  {/*<Router> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
    <Alert alert={alert} />
+  
     <div className='container my-3'> 
-      <Routes> <Route exact path="/about" element={<About />} /> 
-      <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" />} /> 
-      </Routes> </div> </Router> ); }
-
+     {/*<Route> <Route exact path="/about" element={<About />} />  </Route> */}
+      {/*<Route exact path="/" > */}
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" />
+     {/* </Route>  */}
+      </div> 
+      {/*</Router> ); */}
+      </>
+      );
+    }
 export default App;
